0bfe2252160c5e21eeb35400ee9f4610df399a85
Sagar D <37168259+sagarknt97@users.noreply.github.com>
2022-06-06 17:10:49 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,31 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+
+
+def get_response(usrText):
+    bot = ChatBot('Bot',
+                  storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch'
+        },
+        {
+            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+            'threshold': 0.70,
+            'default_response': 'ಕ್ಷಮಿಸಿ ನನಗೆ ಏನು ಅರ್ಥವಾಗುತ್ತಿಲ್ಲ'
+        }
+    ],
+    trainer='chatterbot.trainers.ListTrainer')
+    bot.set_trainer(ListTrainer)
+    while True:
+        if usrText.strip()!= 'Bye':
+            result = bot.get_response(usrText)                        
+            reply = str(result)
+            return(reply)
+        if usrText.strip() == 'Bye':
+            return('Bye')
+            break
+        
+
+        
---
+++ server.py
@@ -0,0 +1,44 @@
+﻿from simple_websocket_server import WebSocketServer, WebSocket
+from chatbot import get_response
+import os
+import wikipedia
+
+
+class ChatServer(WebSocket):
+
+    def handleMessage(self):
+        # echo message back to client
+        message = self.data
+        
+               
+               
+        if message ==  "ನೋಟ್ ಪ್ಯಾಡ್ ಅನ್ನು ತೆರೆ" :
+           os.startfile('C:/WINDOWS/system32/notepad.exe')
+           
+          
+        if message ==  "ಪವರ್ಪಾಯಿಂಟ್ ಅನ್ನು ತೆರೆ" :
+          os.startfile('C:/Program Files/Microsoft Office/Office16/POWERPNT.exe')
+        if message ==  "ಬ್ರೌಸರ್ ಅನ್ನು ತೆರೆ" :
+          os.startfile('C:/Program Files/Mozilla Firefox/firefox.exe')
+        if message ==  "ಎಕ್ಲಿಪ್ಸ್ ಅನ್ನು ತೆರೆ" :
+          os.startfile('C:/Users/Sagar/eclipse/jee-photon/eclipse/eclipse.exe')
+          response = get_response(message)
+          self.sendMessage(response)
+        
+          
+        else:
+         #wikipedia.set_lang("kn")
+         #self.sendMessage(wikipedia.summary(message,senetences=2))
+         response = get_response(message)
+         self.sendMessage(response)
+
+    def handleConnected(self):
+        print(self.address, 'connected')
+
+    def handleClose(self):
+        print(self.address, 'closed')
+
+
+
+server = WebSocketServer('', 8000, ChatServer)
+server.serveforever()
